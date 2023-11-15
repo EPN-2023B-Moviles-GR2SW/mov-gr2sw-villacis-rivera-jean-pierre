ab2f23b2de215c646bec12fb569de293cad2020d
JeanPierreVillacis <jean.villacis@epn.edu.ec>
2023-11-15 07:50:53 -0500

    Add GradleMigrationSettings component and update Main.kt with variable examples

--- 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
+++ 01 - KOTLIN/proyecto/src/main/kotlin/Main.kt
@@ -1,3 +1,28 @@
-fun main(args:Array<String>){
+import java.util.Date
+
+fun main(){
     println("Hello World!")
+    //INMUTABLES (No se reasignan "=")
+    val inmutable: String = "Jean";
+    //inmutable = "Pierre"
+
+    // Mutables (Re asignar)
+    var mutable: String = "Pierre";
+    mutable = "Adrian";
+
+// val > var
+    // Duck Typing
+    var ejemploVariable = "Jean Villacis"
+    val edadEjemplo: Int = 12
+    ejemploVariable.trim()
+    //ejemploVariable = edadEjemplo;
+
+    // Varieble primitiva
+    val nombreProfesor: String = "Adrian Eguez"
+    val sueldo: Double = 1.2
+    val estadoCivil: Char ='C'
+    val mayorEdad: Boolean = true
+
+    //Clases Java
+    val fechaNacimiento: Date = Date()
 }
