3a024146de6c5493e163704ab23e514f05bbfd2f
JeanPierreVillacis <jean.villacis@epn.edu.ec>
2023-12-06 09:33:39 -0500

    Actividades Android

--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.b2023gr2sw
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +13,23 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.b2023gr2sw.R.id.btn_ciclo_vida
 import com.example.b2023gr2sw.ui.theme.B2023gr2swTheme
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView (R.layout.activity_main)
+        val botonCicloVida = findViewById<Button>(btn_ciclo_vida)
+        botonCicloVida
+            .setOnClickListener{
+                irActividad(ACicloVida::class.java)
+            }
+    }
+    fun irActividad(
+        clase:Class<*>
+    ){
+        val intent = Intent(this,clase)
+        startActivity(intent)
     }
 }
