9e0548d030cb7b82693d8393d4d7edfbab94df57
JeanPierreVillacis <jean.villacis@epn.edu.ec>
2023-12-20 09:48:31 -0500

    Clase 20 de diciembre

---
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/CIntentExplicitoParametros.kt
@@ -0,0 +1,28 @@
+package com.example.b2023gr2sw
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class CIntentExplicitoParametros : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cintent_explicito_parametros)
+        val nombre = intent.getStringArrayExtra("nombre")
+        val apellido = intent.getStringArrayExtra("apellido")
+        val edad = intent.getIntExtra("edad",0)
+        val boton = findViewById<Button>(R.id.btn_devolver_respuesta)
+        boton .setOnClickListener{devolverRespuesta()}
+    }
+    fun devolverRespuesta(){
+        val intentDevolverParametros = Intent()
+        intentDevolverParametros.putExtra("nombreModificado","Vicente")
+        intentDevolverParametros.putExtra("edadModificado",33)
+        setResult(
+            RESULT_OK,
+            intentDevolverParametros
+        )
+        finish()
+    }
+}
--- 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
+++ 02-Android/app/src/main/java/com/example/b2023gr2sw/MainActivity.kt
@@ -1,22 +1,66 @@
 package com.example.b2023gr2sw
 
+import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.widget.Button
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.b2023gr2sw.R.id.btn_ciclo_vida
-import com.example.b2023gr2sw.ui.theme.B2023gr2swTheme
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    val callbackContenidoIntetExplicito =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if (result.resultCode ==Activity.RESULT_OK){
+                if(result.data!= null) {
+                    //Logica Negocio
+                    val data = result.data
+                    mostrarSnackbar(
+                        "${data?.getStringArrayExtra("nombreModificado")}"
+                    )
+                }
+            }
+        }
+
+    fun mostrarSnackbar(texto:String){
+     Snackbar
+         .make(
+             findViewById(R.id.id_layout_main),//view
+            texto, //texto
+             Snackbar.LENGTH_LONG) //tiempo
+
+    }
+
+    val callbackIntentPickUri =
+        registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult()
+        ){
+            result ->
+            if (result.resultCode == RESULT_OK){
+                    if (result.data != null){
+                        if (result.data !!.data != null){
+                            val uri:Uri = result.data!!.data!!
+                            val cursor = contentResolver.query(
+                                uri,null,null,null,null)
+                            cursor?.moveToFirst()
+                            val indiceTelefono = cursor?.getColumnIndex(
+                                ContactsContract.CommonDataKinds.Phone.NUMBER
+                            )
+                            val telefono = cursor?.getString(indiceTelefono!!)
+                            cursor?.close()
+                            mostrarSnackbar("Telefono ${telefono}")
+                        }
+                    }
+
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView (R.layout.activity_main)
@@ -30,7 +74,37 @@ class MainActivity : AppCompatActivity() {
             .setOnClickListener {
                 irActividad(BListView::class.java)
             }
+        val botonIntentImplicito = findViewById<Button>(
+            R.id.btn_ir_intent_implicito)
+        botonIntentImplicito
+            .setOnClickListener{
+                val intentConRespuesta = Intent(
+                    Intent.ACTION_PICK,
+                    ContactsContract.CommonDataKinds.Phone.CONTENT_URI
+                )
+                callbackIntentPickUri.launch(intentConRespuesta)
+            }
+
+        val botonIntentExplicito = findViewById<Button>(
+            R.id.btn_ir_intent_explicito)
+        botonIntentExplicito
+            .setOnClickListener {
+                abrirActividadConParametros(
+                    CIntentExplicitoParametros::class.java)
+
+            }
     }
+   fun abrirActividadConParametros(
+       clase: Class<*>
+   ){
+       val intentExplicito = Intent(this,clase)
+       //Enviar parametros solamente variables primitivas
+       intentExplicito.putExtra("nombre","Adrian")
+       intentExplicito.putExtra("apellido","Eguez")
+       intentExplicito.putExtra("edad",34)
+       callbackContenidoIntetExplicito.launch(intentExplicito)
+   }
+
     fun irActividad(
         clase:Class<*>
     ){
