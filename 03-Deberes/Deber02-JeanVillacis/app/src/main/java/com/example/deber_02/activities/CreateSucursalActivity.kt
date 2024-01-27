67b9040d17a7b5563f320079fe5e9143638dfdf9
JeanPierreVillacis <jean.villacis@epn.edu.ec>
2024-01-27 18:04:41 -0500

    Add Git configuration file and fix go back button in CreateSucursalActivity

--- 03-Deberes/Deber02-JeanVillacis/app/src/main/java/com/example/deber_02/activities/CreateSucursalActivity.kt
+++ 03-Deberes/Deber02-JeanVillacis/app/src/main/java/com/example/deber_02/activities/CreateSucursalActivity.kt
@@ -14,6 +14,7 @@ class CreateSucursalActivity : AppCompatActivity() {
         setContentView(R.layout.activity_create_sucursal)
 
         val goBackButton = findViewById<ImageButton>(
+            
             R.id.go_back_to_supermercados_list
         )
 
