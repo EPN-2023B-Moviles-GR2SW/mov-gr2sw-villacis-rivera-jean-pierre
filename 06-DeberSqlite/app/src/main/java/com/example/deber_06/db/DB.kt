9a1983ce358031eb12a71f96931765f2674398a8
Jean Pierre Villacis <103060727+JeanPierreVillacis@users.noreply.github.com>
2024-01-27 17:10:12 -0500

    Delete 06-DeberSqlite directory

--- 06-DeberSqlite/app/src/main/java/com/example/deber_06/db/DB.kt
+++
@@ -1,8 +0,0 @@
-package com.example.deber_06.db
-
-class DB {
-    companion object {
-        var supermercados: SqliteHelperSupermercado? = null
-        var sucursales: SqliteHelperSucursal? = null
-    }
-}
